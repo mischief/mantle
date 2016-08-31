fac77cdb1e825a14a0c13b0f0d1fdc41d3f4708e
Nick Owens <mischief@coreos.com>
2016-08-30 21:48:28 -0700

    platform/api/azure: add replication binding

---
+++ platform/api/azure/replication.go
@@ -0,0 +1,29 @@
+// Copyright 2016 CoreOS, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package azure
+
+import (
+	"github.com/Azure/azure-sdk-for-go/management/osimage"
+)
+
+func (a *API) ReplicateImage(image, offer, sku, version string, regions ...string) error {
+	c := osimage.NewClient(a.client)
+	op, err := c.ReplicateImage(image, offer, sku, version, regions...)
+	if err != nil {
+		return err
+	}
+
+	return a.client.WaitForOperation(op, nil)
+}
